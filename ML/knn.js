bfe8116539b3abace4013c0cf6b67e8b67ba4467
AkhilHector <akhilhector.1@gmail.com>
2016-10-12 23:31:59 +0530

    Added normalizing steps for the algorithm
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- ML/knn.js
+++ ML/knn.js
@@ -11,8 +11,28 @@ var node = function (foo) {
 }
 
 var nodeList = function (mysterypoint) {
-  this.node = []
+  this.nodes = []
   this.mysterypoint = mysterypoint
+}
+
+nodeList.prototype.methodName = function () {
+  this.areas = {min: 1000, max: 0}
+  this.rooms = {min: 1000, max: 0}
+
+  for(var i in this.nodes) {
+    if(this.nodes[i].rooms < this.rooms.min) {
+        this.rooms.min = this.nodes[i].rooms
+    }
+    if(this.nodes[i].rooms > this.rooms.max) {
+       this.rooms.max = this.nodes[i].rooms
+    }
+    if(this.nodes[i].areas < this.areas.min) {
+        this.areas.min = this.nodes[i].min
+    }
+    if(this.nodes[i].areas > this.areas.max) {
+        this.areas.max = this.nodes[i].max
+    }
+  }
 }
 
 var test = {first: "fargument", second: "sargument"}
