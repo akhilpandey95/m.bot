1057027754c5d2598f0abedd5a56af042f4287f3
AkhilHector <akhilhector.1@gmail.com>
2017-03-25 17:51:53 +0530

    Minor type in the send news function
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- app.js
+++ app.js
@@ -129,140 +129,141 @@ function sendNews(recipientId) {
                         ]
                     }
                 }
-            };
-            callSendAPI(msgData);
-        } else {
-            let msgData = {
-                recipient: {
-                    id: recipientId
-                },
-                message: {
-                    text: "Hello there, you cannot ask me to report. :P",
-                    metadata: "DEV_META_DATA_REPORT_USER"
-                }
-            };
-            callSendAPI(msgData);
-        }
+            }
+        };
+        callSendAPI(msgData);
+    } else {
+        let msgData = {
+            recipient: {
+                id: recipientId
+            },
+            message: {
+                text: "Hello there, you cannot ask me to report. :P",
+                metadata: "DEV_META_DATA_REPORT_USER"
+            }
+        };
+        callSendAPI(msgData);
     }
+}
 
-    function callSendAPI(msgData) {
-        r ({
-            uri: c.felix_fb.api_url,
-            qs: {access_token: ACC_TOKEN},
-            method: 'POST',
-            json: msgData
-        }, function (err, res, body) {
-            if(!err && res.statusCode == 200) {
-                var recipientId = body.recipient_id;
-                var messageId = body.message_id;
+function callSendAPI(msgData) {
+    r ({
+        uri: c.felix_fb.api_url,
+        qs: {access_token: ACC_TOKEN},
+        method: 'POST',
+        json: msgData
+    }, function (err, res, body) {
+        if(!err && res.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
 
-                if (messageId) {
-                    console.log("Message sent to %s", recipientId)
-                } else {
-                    console.log("Called the send Send API method for %s", recipientId)
-                }
+            if (messageId) {
+                console.log("Message sent to %s", recipientId)
             } else {
-                console.error("Failed calling the Send API", res.statusCode, res.statusMessage)
+                console.log("Called the send Send API method for %s", recipientId)
             }
-        });
-    }
-
-    function verifyRequestSignature(req, res, buf) {
-        var signature = req.headers['x-hub-signature'];
-
-        if (!signature) {
-            console.error("Couldn't validate the signature")
         } else {
-            var elements = signature.split('=')
-            var method = elements[0];
-            var signatureHash = elements[1];
-            var expectedHash = cr.createHmac('sha1', APP_SECRET).update(buf).digest('hex');
-
-            if(signatureHash != expectedHash) {
-                throw new Error("Couldn't validate the signature");
-            }
-        }
-    }
-
-    function receiveMessage(event) {
-        var senderID = event.sender.id;
-        var recipientID = event.recipient.id;
-        var timeOfMessage = event.timestamp;
-        var message = event.message;
-
-        console.log(JSON.stringify(message));
-
-        var isEcho = message.is_echo;
-        var messageId = message.mid;
-        var appId = message.app_id;
-        var metadata = message.metadata;
-        var messageText = message.text;
-        var messageAttachments = message.attachments;
-        var quickReply = message.quick_reply;
-
-        if (isEcho) {
-            console.log(messageId)
-            sendMsg(senderID, "hello there i've heard you");
-        } else if(quickReply) {
-            var quickReplyPayload = quickReply.payload;
-            sendMsg(senderID, "Quick thing tapped")
-            return;
-        }
-
-        if (messageText) {
-            switch (messageText) {
-                case 'report' :
-                sendReportMessage(senderID)
-                break;
-
-                case 'news' :
-                sendNews(senderID)
-                break;
-
-                default:
-                sendMsg(senderID, messageText);
-            }
-        } else if (messageAttachments) {
-            sendMsg(senderID, "Oops, i currently cannot support Stickers :(");
-        }
-    }
-
-    // define the routes now
-    felix.get('/', function (req, res) {
-        res.send('Hello there, this is felix, Cheif Operaional Commandant to Mr Akhil Pandey');
-    });
-
-    //send and receive webhooks
-    felix.get('/facebook', function (req, res) {
-        if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === VAL_TOKEN) {
-            res.status(200).send(req.query['hub.challenge']);
-        }
-        else {
-            console.error("Failed to validate the token")
-            res.sendStatus(403);
+            console.error("Failed calling the Send API", res.statusCode, res.statusMessage)
         }
     });
+}
 
-    felix.post('/facebook', function (req, res) {
-        var data = req.body;
+function verifyRequestSignature(req, res, buf) {
+    var signature = req.headers['x-hub-signature'];
 
-        if (data.object == 'page') {
-            data.entry.forEach(function (pageEntry) {
-                var pageID = pageEntry.id;
-                var timeOfEvent = pageEntry.time;
+    if (!signature) {
+        console.error("Couldn't validate the signature")
+    } else {
+        var elements = signature.split('=')
+        var method = elements[0];
+        var signatureHash = elements[1];
+        var expectedHash = cr.createHmac('sha1', APP_SECRET).update(buf).digest('hex');
 
-                pageEntry.messaging.forEach(function (messagingEvent) {
-                    if(messagingEvent.message) {
-                        receiveMessage(messagingEvent)
-                    }
-                })
+        if(signatureHash != expectedHash) {
+            throw new Error("Couldn't validate the signature");
+        }
+    }
+}
+
+function receiveMessage(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
+
+    console.log(JSON.stringify(message));
+
+    var isEcho = message.is_echo;
+    var messageId = message.mid;
+    var appId = message.app_id;
+    var metadata = message.metadata;
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+    var quickReply = message.quick_reply;
+
+    if (isEcho) {
+        console.log(messageId)
+        sendMsg(senderID, "hello there i've heard you");
+    } else if(quickReply) {
+        var quickReplyPayload = quickReply.payload;
+        sendMsg(senderID, "Quick thing tapped")
+        return;
+    }
+
+    if (messageText) {
+        switch (messageText) {
+            case 'report' :
+            sendReportMessage(senderID)
+            break;
+
+            case 'news' :
+            sendNews(senderID)
+            break;
+
+            default:
+            sendMsg(senderID, messageText);
+        }
+    } else if (messageAttachments) {
+        sendMsg(senderID, "Oops, i currently cannot support Stickers :(");
+    }
+}
+
+// define the routes now
+felix.get('/', function (req, res) {
+    res.send('Hello there, this is felix, Cheif Operaional Commandant to Mr Akhil Pandey');
+});
+
+//send and receive webhooks
+felix.get('/facebook', function (req, res) {
+    if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === VAL_TOKEN) {
+        res.status(200).send(req.query['hub.challenge']);
+    }
+    else {
+        console.error("Failed to validate the token")
+        res.sendStatus(403);
+    }
+});
+
+felix.post('/facebook', function (req, res) {
+    var data = req.body;
+
+    if (data.object == 'page') {
+        data.entry.forEach(function (pageEntry) {
+            var pageID = pageEntry.id;
+            var timeOfEvent = pageEntry.time;
+
+            pageEntry.messaging.forEach(function (messagingEvent) {
+                if(messagingEvent.message) {
+                    receiveMessage(messagingEvent)
+                }
             })
-            res.sendStatus(200);
-        }
-    });
+        })
+        res.sendStatus(200);
+    }
+});
 
-    felix.listen(felix.get('port'), function() {
-        console.log("running node app");
-    });
+felix.listen(felix.get('port'), function() {
+    console.log("running node app");
+});
 
-    module.exports = felix;
+module.exports = felix;
